71fca9f161ed9418b79068a487d4ec5b4c4c70f1
jithinp040 <37218372+jithinp040@users.noreply.github.com>
2018-03-09 21:47:08 +0530

    Create voworcon.py

---
+++ voworcon.py
@@ -0,0 +1,9 @@
+c=['a','e','i','o','u']
+choice='y'
+while(choice=='y'):
+  a=str(input("Enter character to check whether vowel or consonant:"))
+  if(a in c):
+    print("character is vowel")
+  else:
+    print("character is consonant")
+  choice=input("do you want to continue(y/n):")
