e788a199d07d55edb23bda4cfd744dc3602c3971
jithinp040 <37218372+jithinp040@users.noreply.github.com>
2018-03-09 21:15:46 +0530

    Update posorneg.py

--- posorneg.py
+++ posorneg.py
@@ -1,6 +1,11 @@
-a=input()
-int(a)
-if(1<=a<=100000)
-  print("positive number")
-else 
-  print("negative number")
+choice='y'
+while(choice=='y'):
+  a=int(input("Enter to check whether it is +ve or -ve:"))
+  if(1<=a<=100000):
+    print("number is +ve")
+  elif (a==0):
+    print("number is zero")
+  else:
+    print("number is -ve")
+  choice=input("do you want to continue(y/n)")
+
