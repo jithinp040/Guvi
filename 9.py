beaf5143a54f3a19c2884e4bf91da3f62f20faaf
jithinp040 <37218372+jithinp040@users.noreply.github.com>
2018-03-10 14:55:32 +0530

    Create 9.py

---
+++ 9.py
@@ -0,0 +1,11 @@
+print("enter to find sum of integers")
+n=int(input("enter no. of terms"))
+i=0
+a=[int(input()) for i in range(0,n)]
+b=int(input("Enter no of terms to be added:"))
+i=0
+sum=0
+while(i<b):
+  sum=sum+a[i]
+  i=i+1
+print("sum of natural numbers:")+str(sum)
