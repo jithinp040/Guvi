5583defe560a6bbfaaaba34b16b58804df2483a8
jithinp040 <37218372+jithinp040@users.noreply.github.com>
2018-03-10 12:32:34 +0530

    Create alphaornot.py

---
+++ alphaornot.py
@@ -0,0 +1,6 @@
+print("Enter something to check whether it is alphabet or not")
+a=str(raw_input("Enter string:"))
+if(a.isalpha()):
+  print("It is an alphabet")
+else:
+  print("no it is not an alphabet")
