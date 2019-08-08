b4d7353318fdd15ed140cad76bd2986e1d8b6ab9
jithinp040 <37218372+jithinp040@users.noreply.github.com>
2019-08-08 14:56:43 +0530

    Update and rename Check whether odd or even.py to Check-whether-odd-or-even.py

--- Check whether odd or even.py
+++
@@ -1,8 +0,0 @@
-choice='y'
-while(choice=='y'):
-  a=int(input("check whether odd or even:"))
-  if(a%2==0):
-    print("number is even")
-  else:
-    print("number is odd")
-  choice=input("do you want to continue(y/n):")
