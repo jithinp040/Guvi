5ef67a24b4962f220671529c0c07cef2b99601e3
jithinp040 <37218372+jithinp040@users.noreply.github.com>
2019-08-08 14:59:02 +0530

    Update and rename Check alphabet or not.py to Check-alphabet-or-not.py

--- Check alphabet or not.py
+++
@@ -1,6 +0,0 @@
-print("check whether it is alphabet or not")
-a=str(raw_input("Enter string:"))
-if(a.isalpha()):
-  print("It is an alphabet")
-else:
-  print("no it is not an alphabet")
---
+++ Check-alphabet-or-not.py
@@ -0,0 +1,6 @@
+print("check whether it is alphabet or not")
+a=input()
+if(a.isalpha()):
+  print("Alphabet")
+else:
+  print("No")
