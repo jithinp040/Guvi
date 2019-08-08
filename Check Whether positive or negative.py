5687ea658f5f8025d18e0118f76472f48a6c106a
jithinp040 <37218372+jithinp040@users.noreply.github.com>
2019-08-08 14:50:00 +0530

    Update Check Whether positive or negative.py

--- Check Whether positive or negative.py
+++ Check Whether positive or negative.py
@@ -1,11 +1,7 @@
-choice='y'
-while(choice=='y'):
-  a=int(input("check whether it is +ve or -ve:"))
-  if(1<=a<=100000):
-    print("number is +ve")
-  elif (a==0):
-    print("number is zero")
-  else:
-    print("number is -ve")
-  choice=input("do you want to continue(y/n)")
-
+a=int(input())
+if(1<=a<=100000):
+  print("Positive")
+elif (a==0):
+  print("Zero")
+else:
+  print("Negative")
