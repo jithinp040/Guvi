40e5de5b9f57c3fe17d069dbccc53f145715a600
jithinp040 <37218372+jithinp040@users.noreply.github.com>
2019-08-09 15:32:05 +0530

    Update and rename Sum of Natural Numbers.py to Sum-of-Natural-Numbers.py

--- Sum of Natural Numbers.py
+++
@@ -1,8 +0,0 @@
-print("to find sum of natural numbers")
-n=int(input("no. terms"))
-sum=0
-i=0
-while(i<=n):
-  sum=sum+i
-  i=i+1
-print("sum of natural numbers is :") + str(sum)
---
+++ Sum-of-Natural-Numbers.py
@@ -0,0 +1,7 @@
+TermsCount=int(input())
+sum=0
+i=0
+while(i<=TermsCount):
+  sum=sum+i
+  i=i+1
+print(sum)
