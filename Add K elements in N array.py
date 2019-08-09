a3670aef874298f4a9778debb5d99046c1675b36
jithinp040 <37218372+jithinp040@users.noreply.github.com>
2019-08-09 15:36:23 +0530

    Update Add K elements in N array.py

--- Add K elements in N array.py
+++ Add K elements in N array.py
@@ -1,11 +1,8 @@
-print("to find sum of integers")
-n=int(input("enter no. of terms"))
-i=0
-a=[int(input()) for i in range(0,n)]
-b=int(input("Enter no of terms to be added:"))
+n,k = map(int,input().split())
+a = [int(x) for x in input().split()][:n]
 i=0
 sum=0
-while(i<b):
-  sum=sum+a[i]
-  i=i+1
-print("sum of integers:")+str(sum)
+while(i<k):
+    sum=sum+a[i]
+    i=i+1
+print(sum)
